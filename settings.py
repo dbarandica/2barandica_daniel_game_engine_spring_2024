02b0bcf9e2ee96851432c834ba6ca861bfc65478
dbarandica <142930222+dbarandica@users.noreply.github.com>
2024-03-22 10:24:53 -0700

    Add files via upload
    initials

---
+++ settings.py
@@ -0,0 +1,23 @@
+# This file was created by: Daniel Barandica
+# Code from course code files
+
+WIDTH = 1024
+HEIGHT = 768
+
+FPS = 30
+
+TITLE = "My Fun Game"
+
+TILESIZE = 32
+
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BGCOLOR = (0,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+LIGHTGREY = (75,75,75)
+
+# Player settings
+PLAYER_SPEED = 300
