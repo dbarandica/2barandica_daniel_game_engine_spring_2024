3023cda1ec640f541a5d335b277a13a7fdb2023a
dbarandica <142930222+dbarandica@users.noreply.github.com>
2024-04-30 23:09:45 -0700

    attempt at an in game shop, to buy different background colors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # This file was created by: Daniel Barandica
 # Code from course code files
 # import libraries and modules
@@ -19,6 +20,52 @@ class Game:
         # setting game clock 
         self.clock = pg.time.Clock()
         self.load_data()
+        self.MENU_ITEMS = ["Shop, Purple, Blue"] #make a shop where you 
+        self.coins_collected = 0  # Track the coins collected by the player
+        self.background_color = BGCOLOR  # Initialize the background color to default
+
+        self.font = pg.font.Font(None, 36) #any font, with size 36
+        self.current_screen = "Shop" #indicates current location, either menu or item
+        self.clock = pg.time.Clock() #controlls frame rate
+
+    def draw_shop_screen(self):
+        shop_text = self.font.render("Shop!", True, (255, 255, 255)) #makes font color black, can change with RGB numbers, displays greeting
+        self.screen.blit(shop_text, (50, 100)) #vertical or horizontal shifts for the greeting, "welcome"
+
+    def handle_shop_click(self):
+        # Check if the player has enough coins to buy an item
+        if self.coins_collected >= 1:
+            # If the player has 1 or more coins, deduct 1 coin and set background to blue
+            self.coins_collected -= 1
+            self.background_color = BLUE
+        elif self.coins_collected >= 2:
+            # If the player has 2 or more coins, deduct 2 coins and set background to purple
+            self.coins_collected -= 2
+            self.background_color = PURPLE
+
+    def draw_shop_screen(self):
+        shop_text = self.font.render("Shop!", True, (255, 255, 255))
+        shop_rect = shop_text.get_rect()
+        shop_rect.topright = (WIDTH - 10, 10)
+        self.screen.blit(shop_text, shop_rect)
+
+        # Draw the available items in the shop
+        item_text = self.font.render("1 Coin: Blue Background", True, (255, 255, 255))
+        item_rect = item_text.get_rect()
+        item_rect.topleft = (50, 150)
+        self.screen.blit(item_text, item_rect)
+
+        item_text = self.font.render("2 Coins: Purple Background", True, (255, 255, 255))
+        item_rect = item_text.get_rect()
+        item_rect.topleft = (50, 200)
+        self.screen.blit(item_text, item_rect)
+
+         # Check if the mouse is over the shop items
+        if item_rect.collidepoint(pg.mouse.get_pos()):
+            # If clicked, perform an action (e.g., make a purchase)
+            if pg.mouse.get_pressed()[0]:
+                self.handle_shop_click()
+
     def load_data(self):
         game_folder = path.dirname(__file__)
         self.map_data = []
@@ -86,11 +133,13 @@ class Game:
         text_rect.topleft = (x*TILESIZE,y*TILESIZE)
         surface.blit(text_surface, text_rect)
     def draw(self):
+            self.screen.fill(self.background_color)
             self.screen.fill(BGCOLOR)
             self.draw_grid()
             self.all_sprites.draw(self.screen)
             self.draw_text(self.screen, str(self.player.moneybag), 64, WHITE, 1, 1)
             pg.display.flip()
+    
     def events(self):
          for event in pg.event.get():
             if event.type == pg.QUIT:
